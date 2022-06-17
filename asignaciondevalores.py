a05532e4380d264be6fb007fa1afd4d2c61dc070
mibarra24 <marcodzeko@gmail.com>
2022-06-16 18:59:13 -0700

    values doc added

---
+++ asignaciondevalores.py
@@ -0,0 +1,19 @@
+number = 10
+number2 = 10.54
+string = "I am 'studying'"
+value = True 
+
+print(number)
+print(type(number))
+print(number2)
+print(type(number2))
+print(string)
+print(type(string))
+print(value)
+print(type(value))
+
+num1 = 34
+num2 = 13
+result = (num1+num2) * 10/6
+
+print(result)
