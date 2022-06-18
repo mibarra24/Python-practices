0edbc1068092f3b98c768848c0bc25ed3661d6e2
mibarra24 <marcodzeko@gmail.com>
2022-06-17 21:52:43 -0700

    operations doc added

---
+++ comentarios.py
@@ -0,0 +1,12 @@
+# Python allows dinamic typing
+'''
+El tipado dinamico es que una misma variable puede
+contener diferentes tipos de datos a lo largo de un programa
+'''
+value = 10
+
+print(value)
+
+value = "Marco"
+
+print(value)
---
+++ operaciones.py
@@ -0,0 +1,20 @@
+# Operadores aritmeticos
+
+num1 = 10
+num2 = 5
+num3 = 15
+num4 = 4
+
+resultado = num1 + num2
+resultado2 = num1 - num2
+resultado3 = num1 * num2
+resultado4 = num1 / num2
+resultado5 = num4 // num3
+
+
+print(resultado)
+print(resultado2)
+print(resultado3)
+print(resultado4)
+print(resultado5)
+
