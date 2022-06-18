b6342660de1462f29c1be463609129dd6e3bd21d
mibarra24 <marcodzeko@gmail.com>
2022-06-17 22:00:24 -0700

    operaciones.py updated

--- operaciones.py
+++ operaciones.py
@@ -5,16 +5,22 @@ num2 = 5
 num3 = 15
 num4 = 4
 
-resultado = num1 + num2
-resultado2 = num1 - num2
-resultado3 = num1 * num2
-resultado4 = num1 / num2
-resultado5 = num4 // num3
+result = num1 + num2
+result2 = num1 - num2
+result3 = num1 * num2
+result4 = num1 / num2
+result5 = num3 // num4
+result6 = num1 % num4
+result7 = num2 ** num4
+result8 = 3 ** 3 * (13/5 - (2*4))
 
 
-print(resultado)
-print(resultado2)
-print(resultado3)
-print(resultado4)
-print(resultado5)
+print(result)
+print(result2)
+print(result3)
+print(result4)
+print(result5)
+print(result6)
+print(result7)
+print(result8)
 
