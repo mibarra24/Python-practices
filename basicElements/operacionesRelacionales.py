21a82dbe79c7830be3063f2611d3e3b7b8aa5724
mibarra24 <marcodzeko@gmail.com>
2022-06-20 22:40:02 -0700

    new docs added

---
+++ basicElements/operacionesRelacionales.py
@@ -0,0 +1,27 @@
+result = 10 < 20
+
+result1 = 10 > 20
+
+result2 = 10 == 20
+
+result3 = 10 != 20
+
+result4 = 10 <= 20
+
+result5 = 10 <= 10
+
+result7 = 10 >= 20
+
+a = 10
+b = 20
+c = 30
+
+result8 = a+b == c
+
+print(result)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+print(result5)
+print(result7)
