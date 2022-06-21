d5cf8486202462bbc29a9c96e01107ce738be427
mibarra24 <marcodzeko@gmail.com>
2022-06-21 15:28:40 -0700

    edited doc

---
+++ basicElements/operacionesAsignacion.py
@@ -0,0 +1,15 @@
+# Operaciones de asignacion
+
+a = 0
+
+
+a += 5 #suma en asignacion
+a -= 2 #resta en asignacion
+a *= 3 #multiplicacion en asignacion
+a /= 3 #division en asignacion
+a **= 2 #potencia en asignacion
+a %= 2 #modulo en asignacion
+
+
+
+print(a)
