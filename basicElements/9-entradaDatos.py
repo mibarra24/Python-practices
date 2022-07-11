f4c8a3deb258533160be545bc4771703ff859401
mibarra24 <marcodzeko@gmail.com>
2022-07-11 16:02:44 -0700

    doc edited

---
+++ basicElements/9-entradaDatos.py
@@ -0,0 +1,13 @@
+# Entrada de datos
+
+nombre = input("Digite su nombre: ")
+
+print(f"Hola {nombre}")
+
+numero = input("Digite un numero: ")
+
+print(f"El numero es {numero}")
+
+numero = int(input("Digite un numero: "))
+
+print(f"El numero es {numero+1}")
