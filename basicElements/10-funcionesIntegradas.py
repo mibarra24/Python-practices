86cbe92d46a224c8d0926abf87a68c6b3851c593
mibarra24 <marcodzeko@gmail.com>
2022-07-11 20:05:44 -0700

    doc edited

---
+++ basicElements/10-funcionesIntegradas.py
@@ -0,0 +1,17 @@
+# Funciones integradas
+
+n = str(10)
+n2 = bin(10)
+n3 = hex(10)
+n4 = int("0b1010",2)
+n5 = abs(-8)
+n6 = round(5.6)
+n7 = len("Marco")
+
+print(n)
+print(n2)
+print(n3)
+print(n4)
+print(n5)
+print(n6)
+print(n7)
