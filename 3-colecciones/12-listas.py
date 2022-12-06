c7bbc5b287b602c7850bb233bd5b5642f15490b1
mibarra24 <marcodzeko@gmail.com>
2022-12-05 22:24:43 -0700

    new docs added

---
+++ 3-colecciones/12-listas.py
@@ -0,0 +1,6 @@
+# Listas
+
+list = ["Lunes","Martes","Miercoles","Jueves","Viernes", 40, 5.68, [1,2,3], True]
+
+print(list)
+
---
+++ 3-colecciones/13.listasPt2.py
@@ -0,0 +1,15 @@
+list1 = [1,2,3,4,5]
+list2 = [1,2,4,5]
+list3 = [6,7,8]
+
+#Append argrega valor al final
+list1.append(6)
+list1.append("Marco")
+
+#insert agrega en cualquier posicion de lista
+list2.insert(2,3)
+
+#Extiende la lista, agrega elementos al final
+list3.extend([9,10,11])
+
+print(list2)
