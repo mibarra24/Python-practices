7ee0c2a7dd6d636d20e4810d5f161c7e9206263b
mibarra24 <marcodzeko@gmail.com>
2022-12-26 23:53:18 -0700

    New docs

--- 2-condicionales/ejercicios/1-numerosParesEImpares.py
+++ 2-condicionales/ejercicios/1-numerosParesEImpares.py
@@ -4,4 +4,11 @@
 num1 = int(input("Digite un numero: "))
 num2 = int(input("Digite otro numero: "))
 
-
+if num1%2==0 and num2%2==0:
+    print("Ambos pares")
+elif num1%2==0 and num2%2!=0:
+    pront(f"{num1} es par")
+elif num1%2!=0 and num2%2==0:
+    print(f"{num2} es par")
+else:
+    print("Ambos son impares")
---
+++ 2-condicionales/ejercicios/2-mayorDe3Numeros.py
@@ -0,0 +1,6 @@
+# Hacer un progama que pida 3 numeros y determine cual es le mayor 
+
+num1 = int(input("Digite un numero: "))
+num2 = int(input("Digite un numero: "))
+num3 = int(input("Digite un numero: "))
+
